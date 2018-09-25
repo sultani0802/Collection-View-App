05793f18dccd304fa9a68f8f11e27d79fdf4911b
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-25 15:14:58 -0400

    Initial Commit

---
+++ Auto Layout Programmatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Programmatically
+//
+//  Created by Haamed Sultani on 2018-09-25.
+//  Copyright © 2018 Sultani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
