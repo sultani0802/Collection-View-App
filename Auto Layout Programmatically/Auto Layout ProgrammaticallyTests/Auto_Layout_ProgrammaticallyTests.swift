58a74f20fbd659237cd0ea27002b8c6a6e50a3d2
sultani0802 <haamedsultani@cmail.carleton.ca>
2018-09-27 21:21:07 -0400

    Cleaned up left over junk code

---
+++ Auto Layout Programmatically/Auto Layout ProgrammaticallyTests/Auto_Layout_ProgrammaticallyTests.swift
@@ -0,0 +1,18 @@
+//
+//  Auto_Layout_ProgrammaticallyTests.swift
+//  Auto Layout ProgrammaticallyTests
+//
+//  Created by Haamed Sultani on 2018-09-25.
+//  Copyright © 2018 Sultani. All rights reserved.
+//
+
+import XCTest
+@testable import Auto_Layout_Programmatically
+
+class Auto_Layout_ProgrammaticallyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+}
--- Auto Layout Programmatically/Controllers/SwipingController.swift
+++ Auto Layout Programmatically/Controllers/SwipingController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+extension UIColor{
+    static var mainPink = UIColor(red: 232/255, green: 68/255, blue: 133/255, alpha: 1)
+}
+
 
 class SwipingController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
